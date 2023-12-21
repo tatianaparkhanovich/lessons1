504bdc47fd1cdf64a2d7fbf17e980a69ecd31b4a
Tania Parhanovich <tania.tigrovaa@gmail.com>
2023-12-21 13:38:39 +0300

    added jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Main } from "./components/Main/Main";
-import { Header } from "./components/header/Header";
+import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 
 function App() {
